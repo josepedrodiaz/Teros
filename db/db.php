9355d0647248dcf4dce75036c04f9f94cbbe6f4d
josepedrodiaz <josepedrodiaz@gmail.com>
2015-02-07 18:05:04 -0300

    Gitignore test
    gitignore test

--- db/db.php
+++ db/db.php
@@ -5,7 +5,7 @@
 $hostname_db = "localhost";
 $database_db = "teros";
 $username_db = "root";
-$password_db = "Jac6992!c";
+$password_db = "Jac6992!";
 $db = mysql_pconnect($hostname_db, $username_db, $password_db) or trigger_error(mysql_error(),E_USER_ERROR); 
 mysql_select_db($database_db, $db);
 ?>
