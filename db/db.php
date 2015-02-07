387577bcc0dba5c00a579c891813af4587bf42df
lucasedlp <lucasyurkiw@gmail.com>
2015-02-07 00:56:15 -0300

    prueba db
    prueba db

--- db/db.php
+++ db/db.php
@@ -1,4 +1,7 @@
 <?php
+//prueba
+
+
 # FileName="Connection_php_mysql.htm"
 # Type="MYSQL"
 # HTTP="true"
@@ -9,3 +12,4 @@ $password_db = "";
 $db = mysql_pconnect($hostname_db, $username_db, $password_db) or trigger_error(mysql_error(),E_USER_ERROR); 
 mysql_select_db($database_db, $db);
 ?>
+
