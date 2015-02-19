99167bb7d4c749c2905d7eb6905cbbafe86281ed
josepedrodiaz <josepedrodiaz@gmail.com>
2015-02-19 16:59:42 -0300

    listados.php
    Se agrega el archivo "listados.php" que genera los listados de
    asistentes a los lugares a donde pasamos lista

---
+++ terosgulpoeste/listados.php
@@ -0,0 +1,48 @@
+<?php 
+require_once('../db/db.php'); 
+
+$id_evento = $_GET["id_evento"];
+
+#GET data de evento
+$query_evento = "SELECT nombre, fecha FROM eventos
+                  WHERE id = $id_evento";
+$result_evento = mysql_query($query_evento, $db) or die(mysql_error());
+while($row = mysql_fetch_array($result_evento)){
+  $nombre_evento = $row["nombre"];
+  $fecha_evento = $row["fecha"];
+}
+mysql_free_result($result_evento);
+
+
+#GEt listado de asistentes
+$query = "SELECT b.nombres AS nombres, b.apellidos AS apellidos, b.dni AS dni 
+          FROM asistencia a, beneficiarios b
+          WHERE a.id_evento = $id_evento
+          AND a.dni = b.dni
+          ORDER BY b.apellidos ASC";
+$result = mysql_query($query) or die(mysql_error());
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Listado</title>
+</head>
+
+<body>
+<h1>EVENTO: <?=$nombre_evento?> (<?=$fecha_evento?>)</h1>
+<table>
+<?php
+while($row = mysql_fetch_array($result)){ #recorre el arreglo de resultados y arma la tabla
+  ?>
+
+<tr><td><?=$row["apellidos"]?></td><td><?=$row["nombres"]?></td><td><?=$row["dni"]?></td></tr>
+<?php
+}
+?>
+</table>
+</body>
+
+</html>
+<?php
+mysql_free_result($result);
+?>
